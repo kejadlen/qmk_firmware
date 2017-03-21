20c5aee33c76caea4ddd2c634b616a836c5b6c24
Alpha Chen <alpha.chen@gmail.com>
2017-03-21 08:23:12 -0700

    [kejadlen] attempt at fixing OS X sleep/wake

--- layouts/community/ergodox/alphadox/keymap.c
+++ layouts/community/ergodox/alphadox/keymap.c
@@ -27,9 +27,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
               KC_Y,   KC_N,           KC_I,            KC_O,     KC_H,     KC_QUOT,
     KC_VOLD,  KC_P,   KC_M,           KC_COMM,         KC_DOT,   KC_SLSH,  KC_RSFT,
                       GUI_T(KC_TAB),  ALT_T(KC_LEFT),  KC_DOWN,  KC_UP,    LCAG_T(KC_RGHT),
-    KC_NO,  KC_NO,
+    KC_WAKE,  KC_PWR,
     KC_NO,
-    KC_NO,  SFT_T(KC_ENT),  LT(SYMB,KC_SPC)
+    KC_NO,    SFT_T(KC_ENT),  LT(SYMB,KC_SPC)
 ),
 
 [SYMB] = LAYOUT_ergodox(
